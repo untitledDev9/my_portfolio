9129158d2bff73d5bc06fb815cb0d8f531908024
untitled <ahmedsuleiman940@gmail.com>
2025-07-23 01:13:44 +0100

    did tablet

--- my-portfolio/src/component/Numbers.jsx
+++ my-portfolio/src/component/Numbers.jsx
@@ -62,23 +62,23 @@ const Experience = () => {
 
   return (
     <div className='width flex items-center justify-center gap-10 text-white mt-14
-    max-mobile:grid max-mobile:grid-cols-2 max-mobile:gap-2max-mobile:place-items-center max-mobile:text-center max-mobile:w-[70%] max-mobile:gap-9 max-mobile:mx-auto 
+    max-tablet:grid max-tablet:grid-cols-2 max-tablet:gap-2max-tablet:place-items-center max-tablet:text-center max-tablet:w-[70%] max-tablet:gap-9 max-tablet:mx-auto 
     '>
-      <div className='gap-2 flex items-center w-[170px] max-mobile:flex-col max-mobile:gap-1 max-mobile:w-[100%]' >
-        <p className='font-bold text-[56px] max-mobile:text-[40px]'>{String(counts.experience).padStart(2, '0')}</p>
-        <p className='w-[60%] text-[#BDBDC1] max-mobile:w-full max-mobile:text-[12px]'>Year(s) of experience</p>
+      <div className='gap-2 flex items-center w-[170px] max-tablet:flex-col max-tablet:gap-1 max-tablet:w-[100%]' >
+        <p className='font-bold text-[56px] max-tablet:text-[40px]'>{String(counts.experience).padStart(2, '0')}</p>
+        <p className='w-[60%] text-[#BDBDC1] max-tablet:w-full max-tablet:text-[12px]'>Year(s) of experience</p>
       </div>
-      <div className='gap-2 flex items-center w-[170px] max-mobile:flex-col max-mobile:gap-1 max-mobile:w-[100%] '>
-        <p className='font-bold text-[56px] max-mobile:text-[40px]'>{String(counts.projects).padStart(2, '0')}</p>
-        <p className='w-[60%] text-[#BDBDC1] max-mobile:w-full max-mobile:text-[12px]'>Projects completed</p>
+      <div className='gap-2 flex items-center w-[170px] max-tablet:flex-col max-tablet:gap-1 max-tablet:w-[100%] '>
+        <p className='font-bold text-[56px] max-tablet:text-[40px]'>{String(counts.projects).padStart(2, '0')}</p>
+        <p className='w-[60%] text-[#BDBDC1] max-tablet:w-full max-tablet:text-[12px]'>Projects completed</p>
       </div>
-      <div className='gap-2 flex items-center w-[170px] max-mobile:flex-col max-mobile:gap-1 max-mobile:w-[100%]'>
-        <p className='font-bold text-[56px] max-mobile:text-[40px]'>{String(counts.tech).padStart(2, '0')}</p>
-        <p className='w-[60%] text-[#BDBDC1] max-mobile:w-full max-mobile:text-[12px]'>Technologies mastered</p>
+      <div className='gap-2 flex items-center w-[170px] max-tablet:flex-col max-tablet:gap-1 max-tablet:w-[100%]'>
+        <p className='font-bold text-[56px] max-tablet:text-[40px]'>{String(counts.tech).padStart(2, '0')}</p>
+        <p className='w-[60%] text-[#BDBDC1] max-tablet:w-full max-tablet:text-[12px]'>Technologies mastered</p>
       </div>
-      <div className='gap-2 flex items-center w-[170px] max-mobile:flex-col max-mobile:gap-1 max-mobile:w-[100%]'>
-        <p className='font-bold text-[56px] max-mobile:text-[40px]'>{commit}</p>
-        <p className='w-[60%] text-[#BDBDC1] max-mobile:w-full max-mobile:text-[12px] max-mobile:mt-'>Code commits </p>
+      <div className='gap-2 flex items-center w-[170px] max-tablet:flex-col max-tablet:gap-1 max-tablet:w-[100%]'>
+        <p className='font-bold text-[56px] max-tablet:text-[40px]'>{commit}</p>
+        <p className='w-[60%] text-[#BDBDC1] max-tablet:w-full max-tablet:text-[12px] max-tablet:mt-'>Code commits </p>
       </div>
     </div>
   );
--- my-portfolio/src/component/Projects.jsx
+++ my-portfolio/src/component/Projects.jsx
@@ -12,14 +12,14 @@ import piggy2 from '../assets/piggy-mobile.png'
 
 const Projects = () => {
   return (
-    <div className=' flex-col flex gap-30'>
+    <div className=' flex-col flex gap-30 max-tablet:gap-22'>
 
 
 
 
       <a href="https://movie-api-delta-dun.vercel.app/" target='blank'>
-        <div className=' width flex gap-3'>
-          <div className='w-[50%]  flex flex-col gap-5'>
+        <div className=' width flex gap-3 max-tablet:flex-col'>
+          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full'>
             <h2 className=" font-bold text-6xl text-transparent stroke-text">01</h2>
             <p className=' font-semibold text-white text-[30px]'>Netflix Clone</p>
             <p className=' leading-7 text-[15px] text-[#BDBDC1]'> A Netflix clone, with ability to search and navigate through all movies</p>
@@ -29,16 +29,18 @@ const Projects = () => {
               <p>Owl-carousel</p>
             </div>
             <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5'>
+            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3'>
               <a href="https://movie-api-delta-dun.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><BsArrowUpRight size={23} /></div></a>
               <a href="https://github.com/untitledDev9/movie-api"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><FaGithub size={23} /></div></a>
             </div>
           </div>
-          <div className='flex items-center justify-end bg-[#266361] relative max-Laptop:bg-amber-700 max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out'>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px]'>
+          <div className='flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
+          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0'
+          >
+            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
               <img src={Movie} alt="" />
             </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px]'>
+            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
               <img src={Movie2} alt="" />
             </div>
           </div>
@@ -49,8 +51,8 @@ const Projects = () => {
 
 
       <a href="https://virtual-r-vert.vercel.app/" target='blank'>
-        <div className=' width flex gap-3'>
-          <div className='w-[50%]  flex flex-col gap-5'>
+        <div className=' width flex gap-3 max-tablet:flex-col'>
+          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full'>
             <h2 className=" font-bold text-6xl text-transparent stroke-text">02</h2>
             <p className=' font-semibold text-white text-[30px]'>Landing Page</p>
             <p className=' leading-7 text-[15px] text-[#BDBDC1]'>Built a simple Landing page that improved my knowledge on CSS grid</p>
@@ -60,16 +62,18 @@ const Projects = () => {
               
             </div>
             <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5'>
+            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3'>
               <a href="https://virtual-r-vert.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><BsArrowUpRight size={23} /></div></a>
               <a href="https://github.com/untitledDev9/VirtualR"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><FaGithub size={23} /></div></a>
             </div>
           </div>
-          <div className='flex items-center justify-end bg-[#633626] relative max-Laptop:bg-amber-700 max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out'>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px]'>
+          <div className='flex items-center justify-end bg-[#633626] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
+          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
+          '>
+            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
               <img src={virtual} alt="" />
             </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px]'>
+            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
               <img src={virtual2} alt="" />
             </div>
           </div>
@@ -79,8 +83,8 @@ const Projects = () => {
 
 
       <a href="https://piggy-vest-full-function.vercel.app/" target='blank'>
-        <div className=' width flex gap-3'>
-          <div className='w-[50%]  flex flex-col gap-5'>
+        <div className=' width flex gap-3 max-tablet:flex-col'>
+          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full'>
             <h2 className=" font-bold text-6xl text-transparent stroke-text">03</h2>
             <p className=' font-semibold text-white text-[30px]'>PiggyVest</p>
             <p className=' leading-7 text-[15px] text-[#BDBDC1]'>Cloned a Real life website, to build more knowledge on how Pro Devs create websites</p>
@@ -90,16 +94,18 @@ const Projects = () => {
               <p>React State</p>
             </div>
             <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5'>
+            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3'>
               <a href="https://piggy-vest-full-function.vercel.app/"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><BsArrowUpRight size={23} /></div></a>
               <a href="https://github.com/untitledDev9/piggyVest-full-function"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><FaGithub size={23} /></div></a>
             </div>
           </div>
-          <div className='flex items-center justify-end bg-[#23276d] relative max-Laptop:bg-amber-700 max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out'>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px]'>
+          <div className='flex items-center justify-end bg-[#23276d] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
+          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
+          '>
+            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
               <img src={piggy} alt="" />
             </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px]'>
+            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
               <img src={piggy2} alt="" />
             </div>
           </div>
@@ -109,9 +115,9 @@ const Projects = () => {
 
 
       <a href="https://country-api-gray.vercel.app/" target='blank'>
-        <div className=' width flex gap-3'>
-          <div className='w-[50%]  flex flex-col gap-5'>
-            <h2 className=" font-bold text-6xl text-transparent stroke-text">03</h2>
+        <div className=' width flex gap-3 max-tablet:flex-col'>
+          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full'>
+            <h2 className=" font-bold text-6xl text-transparent stroke-text">04</h2>
             <p className=' font-semibold text-white text-[30px]'>Countries API</p>
             <p className=' leading-7 text-[15px] text-[#BDBDC1]'>Worked with APIs, and had better understanding on Array methods</p>
             <div className=' flex gap-3 text-[#08c17a]'>
@@ -120,16 +126,18 @@ const Projects = () => {
               <p>APIs</p>
             </div>
             <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5'>
+            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3'>
               <a href="https://country-api-gray.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><BsArrowUpRight size={23} /></div></a>
               <a href="https://github.com/untitledDev9/Country-Api"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3 ' ><FaGithub size={23} /></div></a>
             </div>
           </div>
-          <div className='flex items-center justify-end bg-[#236d27] relative max-Laptop:bg-amber-700 max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out'>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px]'>
+          <div className='flex items-center justify-end bg-[#236d27] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
+          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
+          '>
+            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
               <img src={country} alt="" />
             </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px]'>
+            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
               <img src={country2} alt="" />
             </div>
           </div>
