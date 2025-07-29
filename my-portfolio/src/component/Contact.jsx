67f10bbf85cc72f817c6cf2bc59d0463eb7f2331
untitled <ahmedsuleiman940@gmail.com>
2025-07-29 12:39:40 +0100

    make changes to project componet and contact

--- my-portfolio/src/component/Contact.jsx
+++ my-portfolio/src/component/Contact.jsx
@@ -1,70 +1,83 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { FaPhoneAlt } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { FaLocationDot } from "react-icons/fa6";
+import { FaLocationDot } from "react-icons/fa6"
+
 const Contact = () => {
+  const formRef = useRef();
+
+const sendEmail = async (e) => {
+  e.preventDefault();
+
+  try {
+    const result = await emailjs.sendForm(
+      'service_zy6qwcx',
+      'template_j59sla6',
+      formRef.current,
+      '8z-uHkOu9fhzu68wx'
+    );
+    console.log(result.text);
+    alert("Message sent!");
+  } catch (error) {
+    console.error(error);
+    if (error.message?.includes('Failed to fetch')) {
+      alert("No internet connection. Please try again.");
+    } else {
+      alert("Something went wrong. Please check your form or API keys.");
+    }
+  }
+
+  e.target.reset();
+};
+
+
   return (
-    <div id='contact' className='width my-18 flex items-center justify-center gap-16 max-Laptop:gap-7
-      max-tablet:flex-col-reverse max-tablet:gap-16 
-    '>
-      <div className=' bg-[#313036] w-[50%] p-6 flex flex-col gap-10 max-Laptop:w-[70%] max-Laptop:gap-5
-      max-tablet:w-[80%] max-mobile:w-full
-      '>
-        <div className=' flex flex-col gap-3'>
-          <h2 className=' text-[#00FD9A] text-4xl'>Let's work together</h2>
+    <div id='contact' className='width my-18 flex items-center justify-center gap-16 max-Laptop:gap-7 max-tablet:flex-col-reverse max-tablet:gap-16'>
+      <form ref={formRef} onSubmit={sendEmail} className='bg-[#313036] w-[50%] p-6 flex flex-col gap-10 max-Laptop:w-[70%] max-Laptop:gap-5 max-tablet:w-[80%] max-mobile:w-full'>
+        <div className='flex flex-col gap-3'>
+          <h2 className='text-[#00FD9A] text-4xl'>Let's work together</h2>
           <p className='text-[#BDBDC1] text-[14px] w-[70%] max-mobile:w-full'>"Got an idea, a project, or just want to build something cool together? I'm all ears—let's make it happen."</p>
         </div>
-        <div className=' flex gap-5 flex-col'>
+        <div className='flex gap-5 flex-col'>
           <div className='flex gap-4 max-mobile:flex-col'>
-            <input className=' w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] placeholder:text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="text" placeholder='FirstName' />
-            <input className=' w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] placeholder:text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="text" placeholder='LastName' />
+            <input name='name' className='w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="text" placeholder='First Name' required />
+            <input name='lastName' className='w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="text" placeholder='Last Name' required />
           </div>
           <div className='flex gap-4 max-mobile:flex-col'>
-            <input className=' w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] placeholder:text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="email" placeholder='Email' />
-            <input className=' w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] placeholder:text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="phone" placeholder='Phone Number' />
+            <input name='email' className='w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="email" placeholder='Email' required />
+            <input name='phone' className='w-1/2 bg-[#1C1B22] py-3 max-mobile:w-full pl-3 text-[#BDBDC1] text-[12px] placeholder:text-[11px] rounded' type="tel" placeholder='Phone Number' />
           </div>
-          <select className='bg-[#1C1B22] p-3 text-[#BDBDC1] text-[12px] rounded'>
-            <option value="aaaaaa">Select a service</option>
-            <option value="aaaaaa">aaaaaaaa</option>
-            <option value="aaaaaa">aaaaaaaa</option>
+          <select name='service' className='bg-[#1C1B22] p-3 text-[#BDBDC1] text-[12px] rounded'>
+            <option>Select a service</option>
+            <option>Web Development</option>
+            <option>UI/UX Design</option>
+            <option>gggg</option>
           </select>
-          <textarea placeholder='Type your message here' className='bg-[#1C1B22] text-white p-3 placeholder:text-[#BDBDC1] placeholder:text-[11px] rounded h-44'></textarea>
+          <textarea name='message' placeholder='Type your message here' className='bg-[#1C1B22] text-white p-3 placeholder:text-[#BDBDC1] text-[11px] rounded h-44' required></textarea>
         </div>
-        <button className=' text-black bg-[#00FD9A] py-2 hover:border-2 hover:text-white transition-colors duration-400 ease-in-out hover:border-[#00FD9A] hover:bg-[#00FD9A00]'>Send message</button>
-      </div>
-      <div className=' flex flex-col gap-9 w-[32%] max-Laptop:w-[30%] max-Laptop:gap-6
-      max-tablet:w-[50%] max-mobile:w-full
-      '>
-        <div className=' flex items-center gap-4 max-Laptop:flex-col max-Laptop:items-start max-Laptop:gap-2
-        max-tablet:flex-row max-tablet:gap-5 max-tablet:items-center 
-        '>
-          <div className=' bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><FaPhoneAlt /></div>
-          <div className=' text-white text-[17px] max-Laptop:text-[13px] max-tablet:text-[17px]'>
-            <p className=' text-[#BDBDC1] text-[14px]'>Phone</p>
-            <p>(+234) 909 132 4697</p>
-          </div>
+        <button type='submit' className='text-black bg-[#00FD9A] py-2 hover:border-2 hover:text-white transition-colors duration-400 ease-in-out hover:border-[#00FD9A] hover:bg-[#00FD9A00]'>
+          Send message
+        </button>
+      </form>
+
+      
+      <div className='flex flex-col gap-9 w-[32%] max-Laptop:w-[30%] max-Laptop:gap-6 max-tablet:w-[50%] max-mobile:w-full'>
+        <div className='flex items-center gap-4 ...'>
+          <div className='bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><FaPhoneAlt /></div>
+          <div className='text-white text-[17px]'><p className='text-[#BDBDC1] text-[14px]'>Phone</p><p>(+234) 909 132 4697</p></div>
         </div>
-        <div className=' flex items-center gap-4 max-Laptop:flex-col max-Laptop:items-start max-Laptop:gap-2
-        max-tablet:flex-row max-tablet:gap-5 max-tablet:items-center 
-        '>
-          <div className=' bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><MdEmail /></div>
-          <div className=' text-white text-[17px] max-Laptop:text-[13px] max-tablet:text-[17px]'>
-            <p className=' text-[#BDBDC1] text-[14px]'>Email</p>
-            <p>ahmedsuleiman940@gmail.com</p>
-          </div>
+        <div className='flex items-center gap-4 ...'>
+          <div className='bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><MdEmail /></div>
+          <div className='text-white text-[17px]'><p className='text-[#BDBDC1] text-[14px]'>Email</p><p>ahmedsuleiman940@gmail.com</p></div>
         </div>
-        <div className=' flex items-center gap-4 max-Laptop:flex-col max-Laptop:items-start max-Laptop:gap-2
-        max-tablet:flex-row max-tablet:gap-5 max-tablet:items-center 
-        '>
-          <div className=' bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><FaLocationDot /></div>
-          <div className=' text-white text-[17px] max-Laptop:text-[13px] max-tablet:text-[17px]'>
-            <p className=' text-[#BDBDC1] text-[14px]'>Address</p>
-            <p>Lagos, Nigeria.</p>
-          </div>
+        <div className='flex items-center gap-4 ...'>
+          <div className='bg-[#313036e3] p-3 text-[#00FD9A] text-xl'><FaLocationDot /></div>
+          <div className='text-white text-[17px]'><p className='text-[#BDBDC1] text-[14px]'>Address</p><p>Lagos, Nigeria.</p></div>
         </div>
       </div>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
--- my-portfolio/src/component/Projects.jsx
+++ my-portfolio/src/component/Projects.jsx
@@ -16,133 +16,180 @@ const Projects = () => {
 
 
 
-
-      <a href="https://movie-api-delta-dun.vercel.app/" target='blank'>
-        <div className=' width flex gap-3 max-tablet:flex-col'>
-          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3'>
-            <h2 className=" font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">01</h2>
-            <p className=' font-semibold text-white text-[30px] max-mobile:text-[24px]'>Netflix Clone</p>
-            <p className=' leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6'> A Netflix clone, with ability to search and navigate through all movies</p>
-            <div className=' flex gap-3 text-[#08c17a] max-mobile:text-[13px]'>
-              <p>React.js,</p>
-              <p>Tailwind.css,</p>
-              <p>Owl-carousel</p>
-            </div>
-            <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0'>
-              <a href="https://movie-api-delta-dun.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><BsArrowUpRight size={23} /></div></a>
-              <a href="https://github.com/untitledDev9/movie-api"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><FaGithub size={23} /></div></a>
-            </div>
+      <div
+        className="cursor-pointer width flex gap-3 max-tablet:flex-col"
+      >
+        <div className="w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3">
+          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">01</h2>
+          <p className="font-semibold text-white text-[30px] max-mobile:text-[24px]">Netflix Clone</p>
+          <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
+            A Netflix clone, with ability to search and navigate through all movies
+          </p>
+          <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
+            <p>React.js,</p>
+            <p>Tailwind.css,</p>
+            <p>Owl-carousel</p>
           </div>
-          <div className='flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
-          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0'
-          >
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
-              <img src={Movie} alt="" />
-            </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
-              <img src={Movie2} alt="" />
-            </div>
-          </div>
-        </div>
-      </a>
-
-
-
-
-      <a href="https://virtual-r-vert.vercel.app/" target='blank'>
-        <div className=' width flex gap-3 max-tablet:flex-col'>
-          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3'>
-            <h2 className=" font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">02</h2>
-            <p className='max-mobile:text-[24px] font-semibold text-white text-[30px]'>Landing Page</p>
-            <p className='max-mobile:text-[12px] max-mobile:leading-6 leading-7 text-[15px] text-[#BDBDC1]'>Built a simple Landing page that improved my knowledge on CSS grid</p>
-            <div className=' flex gap-3 text-[#08c17a] max-mobile:text-[13px]'>
-              <p>React.js,</p>
-              <p>Tailwind.css</p>
-              
-            </div>
-            <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0'>
-              <a href="https://virtual-r-vert.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><BsArrowUpRight size={23} /></div></a>
-              <a href="https://github.com/untitledDev9/VirtualR"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><FaGithub size={23} /></div></a>
-            </div>
-          </div>
-          <div className='flex items-center justify-end bg-[#633626] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
-          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
-          '>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
-              <img src={virtual} alt="" />
-            </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
-              <img src={virtual2} alt="" />
-            </div>
+          <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
+          <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
+            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <BsArrowUpRight size={23} />
+              </div>
+            </a>
+            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <FaGithub size={23} />
+              </div>
+            </a>
           </div>
         </div>
-      </a>
+        <div
+          className="flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
+    max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0"
+          onClick={() => window.open("https://movie-api-delta-dun.vercel.app/", "_blank")}
+        >
+          <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
+            <img src={Movie} alt="" />
+          </div>
+          <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
+            <img src={Movie2} alt="" />
+          </div>
+        </div>
+      </div>
 
 
 
-      <a href="https://piggy-vest-full-function.vercel.app/" target='blank'>
-        <div className=' width flex gap-3 max-tablet:flex-col'>
-          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3'>
-            <h2 className=" font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">03</h2>
-            <p className='max-mobile:text-[24px] font-semibold text-white text-[30px]'>PiggyVest</p>
-            <p className='max-mobile:text-[12px] max-mobile:leading-6 leading-7 text-[15px] text-[#BDBDC1]'>Cloned a Real life website, to build more knowledge on how Pro Devs create websites</p>
-            <div className=' flex gap-3 text-[#08c17a] max-mobile:text-[13px]'>
-              <p>React.js,</p>
+      <div
+        className="cursor-pointer width flex gap-3 max-tablet:flex-col"
+      >
+        <div className="w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3">
+          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">02</h2>
+          <p className="font-semibold text-white text-[30px] max-mobile:text-[24px]">Landing Page</p>
+          <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
+            Built a simple Landing page that improved my knowledge on CSS grid
+          </p>
+          <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
+            <p>React.js,</p>
+            <p>Tailwind.css</p>
+          </div>
+          <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
+          <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
+            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <BsArrowUpRight size={23} />
+              </div>
+            </a>
+            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <FaGithub size={23} />
+              </div>
+            </a>
+          </div>
+        </div>
+        <div
+          className="flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
+    max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0"
+          onClick={() => window.open("https://movie-api-delta-dun.vercel.app/", "_blank")}
+        >
+          <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
+            <img src={virtual} alt="" />
+          </div>
+          <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
+            <img src={virtual2} alt="" />
+          </div>
+        </div>
+      </div>
+
+
+
+      <div
+        className="cursor-pointer width flex gap-3 max-tablet:flex-col"
+      >
+        <div className="w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3">
+          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">03</h2>
+          <p className="font-semibold text-white text-[30px] max-mobile:text-[24px]">PiggyVest</p>
+          <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
+            Cloned a Real life website, to build more knowledge on how Pro Devs create websites
+          </p>
+          <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
+            <p>React.js,</p>
               <p>Tailwind.css,</p>
               <p>React State</p>
-            </div>
-            <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0'>
-              <a href="https://piggy-vest-full-function.vercel.app/"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><BsArrowUpRight size={23} /></div></a>
-              <a href="https://github.com/untitledDev9/piggyVest-full-function"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><FaGithub size={23} /></div></a>
-            </div>
           </div>
-          <div className='flex items-center justify-end bg-[#23276d] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
-          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
-          '>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
-              <img src={piggy} alt="" />
-            </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
-              <img src={piggy2} alt="" />
-            </div>
+          <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
+          <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
+            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <BsArrowUpRight size={23} />
+              </div>
+            </a>
+            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <FaGithub size={23} />
+              </div>
+            </a>
           </div>
         </div>
-      </a>
+        <div
+          className="flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
+    max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0"
+          onClick={() => window.open("https://movie-api-delta-dun.vercel.app/", "_blank")}
+        >
+          <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
+            <img src={piggy} alt="" />
+          </div>
+          <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
+            <img src={piggy2} alt="" />
+          </div>
+        </div>
+      </div>
 
 
 
-      <a href="https://country-api-gray.vercel.app/" target='blank'>
-        <div className=' width flex gap-3 max-tablet:flex-col'>
-          <div className='w-[50%]  flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3'>
-            <h2 className=" font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">04</h2>
-            <p className='max-mobile:text-[24px] font-semibold text-white text-[30px]'>Countries API</p>
-            <p className='max-mobile:text-[12px] max-mobile:leading-6 leading-7 text-[15px] text-[#BDBDC1]'>Worked with APIs, and had better understanding on Array methods</p>
-            <div className=' flex gap-3 text-[#08c17a] max-mobile:text-[13px]'>
-              <p>React.js,</p>
+      <div
+        className="cursor-pointer width flex gap-3 max-tablet:flex-col"
+      >
+        <div className="w-[50%] flex flex-col gap-5 max-tablet:w-full max-mobile:gap-3">
+          <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">04</h2>
+          <p className="font-semibold text-white text-[30px] max-mobile:text-[24px]">Countries API</p>
+          <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
+            Worked with APIs, and had better understanding on Array methods
+          </p>
+          <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
+                <p>React.js,</p>
               <p>Tailwind.css,</p>
               <p>APIs</p>
-            </div>
-            <div className=' w-[95%] h-[1px] bg-[#BDBDC1] mb-3'></div>
-            <div className='  flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0'>
-              <a href="https://country-api-gray.vercel.app/ "><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><BsArrowUpRight size={23} /></div></a>
-              <a href="https://github.com/untitledDev9/Country-Api"><div className='text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3  ' ><FaGithub size={23} /></div></a>
-            </div>
           </div>
-          <div className='flex items-center justify-end bg-[#236d27] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out
-          max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0
-          '>
-            <div className=' w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none'>
-              <img src={country} alt="" />
-            </div>
-            <div className=' w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden'>
-              <img src={country2} alt="" />
-            </div>
+          <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
+          <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
+            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <BsArrowUpRight size={23} />
+              </div>
+            </a>
+            <a href="https://github.com/untitledDev9/movie-api" target="_blank">
+              <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
+                <FaGithub size={23} />
+              </div>
+            </a>
           </div>
         </div>
-      </a>
+        <div
+          className="flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
+    max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0"
+          onClick={() => window.open("https://movie-api-delta-dun.vercel.app/", "_blank")}
+        >
+          <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
+            <img src={country} alt="" />
+          </div>
+          <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
+            <img src={country2} alt="" />
+          </div>
+        </div>
+      </div>
+
+
 
 
 
