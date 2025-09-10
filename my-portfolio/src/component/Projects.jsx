1c89d6b3c7eb0391d3285dc5adee87f87377802a
untitled <ahmedsuleiman940@gmail.com>
2025-09-10 17:51:31 +0100

    added a new project

--- my-portfolio/src/component/Projects.jsx
+++ my-portfolio/src/component/Projects.jsx
@@ -9,6 +9,8 @@ import { BsArrowUpRight } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import piggy from '../assets/piggy-desktop.png'
 import piggy2 from '../assets/piggy-mobile.png'
+import speak from '../assets/speaktribe-desktop.png'
+import speaktribe from '../assets/speaktribe-mobile.png'
 
 const Projects = () => {
   return (
@@ -23,16 +25,18 @@ const Projects = () => {
           <h2 className="font-bold text-6xl text-transparent stroke-text max-mobile:text-5xl">01</h2>
           <p className="font-semibold text-white text-[30px] max-mobile:text-[24px]">Local Language Web-App</p>
           <p className="leading-7 text-[15px] text-[#BDBDC1] max-mobile:text-[12px] max-mobile:leading-6">
-            A Full-Stack Local Learning Language Web-App, to learn local languages e.g yoruba, hausa, igbo.
+            A Full-Stack Local Learning Language Web-App (SpeakTribe), to learn local languages e.g yoruba, hausa, igbo.
           </p>
           <div className="flex gap-3 text-[#08c17a] max-mobile:text-[13px]">
             <p>React<span className='text-white'>.</span>js,</p>
             <p>Tailwind<span className='text-white'>.</span>css,</p>
+            <p>Generative<span className='text-white'>.</span>Ai,</p>
+            <p>Supabase</p>
             <p>Node<span className='text-white'>.</span>js<span className='text-white'>(</span>Express<span className='text-white'>)</span></p>
           </div>
           <div className="w-[95%] h-[1px] bg-[#BDBDC1] mb-3"></div>
           <div className="flex gap-5 max-tablet:mx-auto max-tablet:my-3 max-mobile:my-0">
-            <a href="https://movie-api-delta-dun.vercel.app/" target="_blank">
+            <a href="https://speaktribe-frontend.vercel.app" target="_blank">
               <div className="text-white hover:text-[#00FD9A] bg-[#33333355] rounded-full p-3">
                 <BsArrowUpRight size={23} />
               </div>
@@ -47,13 +51,13 @@ const Projects = () => {
         <div
           className="flex items-center justify-end bg-[#266361] relative max-Laptop:w-[50%] h-fit pt-4 w-[60%] hover:pt-0 transition-all duration-400 ease-in-out 
     max-tablet:w-full max-tablet:justify-center max-tablet:bg-[#3330] max-tablet:transition-none max-tablet:pt-0"
-          onClick={() => window.open("https://movie-api-delta-dun.vercel.app/", "_blank")}
+          onClick={() => window.open("https://speaktribe-frontend.vercel.app", "_blank")}
         >
           <div className="w-[450px] hover:w-[480px] transition-all duration-400 ease-in-out max-Laptop:hover:w-[450px] max-tablet:transition-none">
-            <img src={Movie} alt="" />
+            <img src={speak} alt="" />
           </div>
           <div className="w-[200px] absolute top-10 left-6 max-Laptop:w-[140px] max-tablet:hidden">
-            <img src={Movie2} alt="" />
+            <img src={speaktribe} alt="" />
           </div>
         </div>
       </div>
